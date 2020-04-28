28b1b2242d9c2a70ae6f42bf0c22a4a0d67165f5
jyotishina <sg.magento@salsoft.biz>
2020-04-28 23:09:46 +0400

    address fields

---
+++ Addressfield/Model/Checkout/LayoutProcessorPlugin.php
@@ -0,0 +1,44 @@
+<?php
+namespace Custom\Addressfield\Model\Checkout;
+
+class LayoutProcessorPlugin
+{
+    /**
+     * @param \Magento\Checkout\Block\Checkout\LayoutProcessor $subject
+     * @param array $jsLayout
+     * @return array
+     */
+
+    public function afterProcess(
+        \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
+        array  $jsLayout
+    ) {
+        $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']
+        ['shippingAddress']['children']['shipping-address-fieldset']['children']['street'] = [
+            'component' => 'Magento_Ui/js/form/components/group',
+            'label' => __('Complete Address'), 
+            'required' => true, //turn false because I removed main label
+            'dataScope' => 'shippingAddress.street',
+            'provider' => 'checkoutProvider',
+            'sortOrder' => 70,
+            'type' => 'group',
+            'additionalClasses' => 'street',
+            'children' => [
+                [
+                    'label' => __('Label 1'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '0',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+                
+            ]
+        ];
+        return $jsLayout;
+    }
+}
